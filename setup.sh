451b107f7d4b35a64377e18878f2ef6322ef5ea0
user206957845366721 <2304040@escolas.anchieta.br>
2024-10-23 21:43:13 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prof.massaki@gmail.com\"\n\
+email = \"2304040@escolas.anchieta.br\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
