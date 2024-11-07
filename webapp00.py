c71f544b01409c545319d3b23ba0f1c948862e4d
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-06 21:16:09 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -15,6 +15,6 @@ st.subheader("Selecione as opções desejadas na tabela abaixo:")
 
 st.selectbox("Selecione uma opção:", ["Tabela", "Gráfico"])
 
-st.bar_chart(df, horizontal=True)
+st.bar_chart(df)
 
 # Use st.write("") para adicionar um texto ao seu Web app
