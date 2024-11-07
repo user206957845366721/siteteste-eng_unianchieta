109612d529ae09a6a7a1483d9a8de45cf272c431
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-06 22:10:41 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -16,8 +16,16 @@ st.header("Site de Tabela de Orçamentos - Engenharia Civil 2024")
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Selecione as opções desejadas abaixo:")
 
-df1 = pd.DataFrame(np.random.randn(5, 5), columns=("col %d" % i for i in range(5)))
-df2 = pd.DataFrame(np.random.randn(10,5), columns=("col %d" % i for i in range(5)))
+df1 = pd.DataFrame(
+    [
+        {"command": "np.random.randn", columns=("col %d" % i for i in range(11)},
+    ]
+)
+df2 = pd.DataFrame(
+    [
+        {"command": "np.random.randn", columns=("col %d" % i for i in range(11)},
+    ]
+)
 
 left, right = st.columns(2)
 if left.button("Tabela", use_container_width=True):
