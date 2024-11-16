c1d5e889297d1c03b89bb45e7ab01647fb171540
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-16 16:50:51 +0000

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -3,6 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+from io import BytesIO
+from fpdf import FPDF
 
 st.set_page_config(
     page_title="Calculadora de Orçamentos",
@@ -34,14 +36,24 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-data = st.file_uploader("Faça Upload da Lista.XLSX para envio em lote.", type=["xlsx", "xls"])
-PressBotaoEnviaLISTA = st.button(label = '✔️ ENVIAR PARA LISTA')
-if data is not None:
-    df = pd.read_excel(data)   
-    st.write(df)
-    df.columns = ['C1','C2','C3','C4','C5','C6','C7','C8','C9']
-    st.write(df.columns)
-    st.write(df)
+produtos = [
+    {"id": 1, "nome": "Produto A", "preço": "0.0, "desconto": 0.0},
+    {"id": 2, "nome": "Produto B", "preço": "0.0, "desconto": 0.0},
+    {"id": 3, "nome": "Produto C", "preço": "0.0, "desconto": 0.0},
+]
+
+df = pd.DataFrame(produtos)
+st.write("Tabela de Produtos:"
+df_display = st.dataframe(df)
+
+#data = st.file_uploader("Faça Upload da Lista.XLSX para envio em lote.", type=["xlsx", "xls"])
+#PressBotaoEnviaLISTA = st.button(label = '✔️ ENVIAR PARA LISTA')
+#if data is not None:
+#    df = pd.read_excel(data)   
+#    st.write(df)
+#    df.columns = ['C1','C2','C3','C4','C5','C6','C7','C8','C9']
+#    st.write(df.columns)
+#    st.write(df)
 
 
 #data = st.file_uploader("Carregue o seu orçamento aqui.", type=["xlsx", "xls"])
