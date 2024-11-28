25ff5e716b56488404a5c154c16ac87ea072bf5e
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-27 21:35:12 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -111,7 +111,7 @@ def gerar_pdf(df_com_preços):
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
-    fpdf = FPDF(orientation="landscape", format="A4")
+    fpdf = FPDF(orientation="L", format="A4")
 
     pdf.cell(200, 10, txt="Orçamento de Produtos", ln=True, align='C')
     pdf.ln(10)
