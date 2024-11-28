76d15410022b6435e194da3b345dc16e5f08d3ea
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-27 22:05:56 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -128,7 +128,7 @@ def gerar_pdf(df_com_preços):
     pdf.ln()
 
     for index, row in df_com_preços.iterrows():        
-        pdf.cell(100, 15, row['DESCRIÇÃO'][:30], border=1)
+        pdf.cell(100, 15, row['DESCRIÇÃO'][:100], border=1)
         pdf.cell(30, 15, f"R$ {row['R$']:.2f}", border=1)
         pdf.cell(30, 15, f"{row['DESCONTO']}%", border=1)
         pdf.cell(30, 15, f"{row['QUANTIDADE']}", border=1)
